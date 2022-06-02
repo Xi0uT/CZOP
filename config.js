fdaf23719416ac291d5c3f3a07ad8646c90aace2
Xi0uT <105636117+Xi0uT@users.noreply.github.com>
2022-06-02 19:17:10 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+ "verification": "https://discord.com/api/oauth2/authorize?client_id=971014482178482216&permissions=8&scope=bot",
+ "description": "Its a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "domain": "", // domain
+ "google_analitics": "", // google analitics
+ "token": process.env.TOKEN,
+ "https":"https://", // leave as is
+ "port":"5003",
+
+ "client_id":"", // bot client ID
+ "secret":"" // bot client secret for auth
+
+}
+
+
+// for dashboard, read more on https://github.com/IgorKowalczyk/majobot
